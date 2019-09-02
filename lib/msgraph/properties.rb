bdd068b735f768bd4b57fad9df9fd888c30d6fe2
jinroq <2787780+jinroq@users.noreply.github.com>
2019-09-02 12:33:30 +0900

    Moved class method camel_case_to_snake_case and snake_case_to_camel_case of class Msgraph::Properties to Msgraph::Users::Base

--- lib/msgraph/properties.rb
+++ lib/msgraph/properties.rb
@@ -13,21 +13,5 @@ module Msgraph
       'preferredLanguage',
       'surname',
     ]
-
-    def self.camel_case_to_snake_case(str)
-      return str unless str.is_a?(String)
-
-      first_letter, rest = str.to_s.split("", 2)
-      "#{first_letter}#{rest.gsub(/([A-Z])/, '_\1')}".downcase
-    end
-
-    def self.snake_case_to_camel_case(str)
-      return str unless str.is_a?(String)
-
-      first_letter, rest = str.to_s.split("", 2)
-      cameled_rest = rest.gsub(/_(.)/) { |l| l[1].upcase }
-      first_letter.downcase.concat(cameled_rest)
-    end
-
   end
 end
